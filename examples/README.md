7a949c96215a799972b705b00ff38192a41565c2
Eliza Neights <eneights@gmail.com>
2023-11-30 17:34:38 -0500

    Make sample_source_file_generation.py more robust

--- examples/README.md
+++ examples/README.md
@@ -2,10 +2,44 @@
 
 This directory contains examples of the .yaml and .dat files needed to run `sample_source_file_generation.py`. When `sample_source_file_generation.py` is run, it takes spectral (.yaml & .dat) and lightcurve (.dat) files from the directory specified in the input .yaml file (`example.yaml`) and writes the spectral info from the spectrum .yaml file (if spectrum_type: 'yaml') or the path to the .dat file into the MEGAlib .source file (spectrum_type: 'dat'), as well as the path to the lightcurve .dat file.
 
-`example.yaml` is a sample .yaml file used as input for `sample_source_file_generation.py`. 
+`example.yaml` is a sample .yaml file used as input for `sample_source_file_generation.py`.    
+Parameters to be defined in .yaml file:  
+	input_path -- path to directory housing source inputs (e.g. 'MEGAlib_source_inputs/')   
+	output_path -- path to directory to store .source files (e.g. 'MEGAlib_source_files/')  
+	geometry_path -- path to mass model  
+	spectrum_type -- file type to use for spectrum ('yaml' to use built in MEGAlib spectral models with parameters defined in .yaml files or 'dat' to use .dat files with spectral shape)  
+	mix_or_match -- whether to mix or match lightcurves and spectra ('match' will use lightcurves and spectra from same event, 'mix' will randomly match lightcurves with spectra from input directory)  
+	zenith -- incidence (zenith) angle or list of incidence (zenith) angles (between 0 and 180 where 90 degrees is on-axis)    
+	zenith_min -- minimum incidence (zenith) angle (between 0 and 180 where 90 degrees is on-axis, only used if 'zenith' is not defined)     
+	zenith_max -- maximum incidence (zenith) angle (between 0 and 180 where 90 degrees is on-axis, only used if 'zenith' is not defined)     
+	azimuth -- azimuthal angle or list of azimuthal angles (between 0 and 360)    
+	azimuth_min -- minimum azimuthal angle (between 0 and 360, only used if 'azimuth' is not defined)     
+	azimuth_max -- maximum azimuthal angle (between 0 and 360, only used if 'azimuth' is not defined)     
+	ph_flux -- flux or list of fluxes in ph/cm<sup>2</sup>/s   
+	ph_flux_min -- minimum flux in ph/cm<sup>2</sup>/s (only used if 'ph_flux' is not defined)    
+	ph_flux_max -- maximum flux in ph/cm<sup>2</sup>/s (only used if 'ph_flux' is not defined)    
+	e_flux -- flux or list of fluxes in erg/cm<sup>2</sup>/s (only used if 'ph_flux', 'ph_flux_min', & 'ph_flux_max' are not defined)      
+	e_flux_min -- minimum flux in erg/cm<sup>2</sup>/s (only used if 'e_flux', 'ph_flux', 'ph_flux_min', & 'ph_flux_max' are not defined)    
+	e_flux_max -- maximum flux in erg/cm<sup>2</sup>/s (only used if 'e_flux', 'ph_flux', 'ph_flux_min', & 'ph_flux_max' are not defined)    
+	shield_counts -- whether to include line in .source files to store background shield counts in MEGAlib .sim files ('y' for yes, 'n' for no)   
+	coordinate_system -- Coordinate system of .source files ('local' for detector coordinates or 'galactic' for galactic coordinates), galactic is not yet supported   
 
 The `example-spectraltype_spectrum.yaml` files are examples of each of the MEGAlib spectral models supported by `sample_source_file_generation.py`: monoenergetic, Band function, Comptonized, power law, and broken power law.
 
 `example_spectrum.dat` is a sample spectral .dat file which can be used in place of the .yaml files. This allows for custom spectral shapes to be used instead of the built in MEGAlib functions. Only the shape is important, not the amplitude, since the flux is defined in the .source file. The spectrum files (either .yaml or .dat) will need to be located in the `input_path/event_type/event_subtype` (or `input_path/event_type` if no event subtypes) directory indicated in the input .yaml file.
 
-`example_lightcurve.dat` is a sample lightcurve file. Only the shape is important, not the amplitude, since the flux is defined in the .source file. The lightcurve files need to be in `input_path/event_type/event_subtype` (or `input_path/event_type` if no event subtypes) directory indicated in the input .yaml file.
+`example_lightcurve.dat` is a sample lightcurve file. Only the shape is important, not the amplitude, since the flux is defined in the .source file. The lightcurve files need to be in `input_path/event_type/event_subtype` (or `input_path/event_type` if no event subtypes) directory indicated in the input .yaml file.    
+
+`example_trigger_algorithm.yaml` is a sample .yaml file used as input for `trigger_algorithm_list_generation.py`.    
+Parameters to be defined in .yaml file:    
+	source_path -- path to directory housing source .sim files (e.g. 'MEGAlib_outputs/')  
+	output_path -- path to directory to store output (e.g. 'trigger_inputs/')    
+	geometry_path -- path to geometry file, must match the geometry used to create simulations    
+	background_type -- whether to randomly select background regions for each source ('random') or use same background file for all sources ('file')  
+	background_path -- if background_type is 'random', path to directory housing background .sim files, and if background_type is 'file', the path to the concatenated background file (e.g. 'MEGAlib_backgrounds/')    
+	background_number -- number of background files for each component (only necessary if background_type is 'random')    
+	background_file_length -- length in s of each background file (only necessary if background_type is 'random')    
+	background_time -- amount of background in s to add to each source, source will be added to the middle of the background time interval    
+	background_components -- list of background components to include, file names must begin with component names (only necessary if background_type is 'random')    
+	background_file_type -- whether the background files begin one after another in time ('sequential') or if they begin at the same time ('simultaneous') (only necessary if background_type is 'random')    
+	mass_model_version -- version of mass model    
--- sample_source_file_generation.py
+++ sample_source_file_generation.py
@@ -2,6 +2,7 @@ import argparse
 import yaml
 import os
 import numpy as np
+from scipy import integrate
 
 # Initialize parser & read arguments from command line
 def parse_args():
@@ -20,19 +21,61 @@ def read_yaml(file):
 
 	return inputs
 
-# Define paths to input files, output source files, and current working directory
+# Define paths to input files and output source files
 def define_paths(inputs):
 
-	if 'event_subtype' in inputs:
-		input_path = os.path.abspath(inputs['input_path'] + inputs['event_type'] + '/' + inputs['event_subtype']) + '/'
-		output_path = os.path.abspath(inputs['output_path'] + inputs['event_type'] + '/' + inputs['event_subtype']) + '/'
-	else:
-		input_path = os.path.abspath(inputs['input_path'] + inputs['event_type']) + '/'
-		output_path = os.path.abspath(inputs['output_path'] + inputs['event_type']) + '/'
+	input_path = os.path.abspath(inputs['input_path']) + '/'
+	output_path = os.path.abspath(inputs['output_path']) + '/'
 	if not os.path.isdir(output_path):
 		os.makedirs(output_path)
 
 	return input_path, output_path
+
+# Create README file
+def write_readme(inputs, input_path, output_path):
+
+	with open(output_path + 'README.md', 'w') as f:
+		f.write('These source files were created using the input files in ' + input_path + '. ')
+		
+		if inputs['coordinate_system'] == 'local':
+			if 'zenith' in inputs:
+				if type(inputs['zenith']) == int:
+					f.write('All .source files have a zenith angle of ' + str(inputs['zenith']) + ' degrees. ')
+				else:
+					f.write('The zenith angles of each source were chosen from the following list (in degrees): ' + str(inputs['zenith']) + '. ')
+			elif 'zenith_min' in inputs and 'zenith_max' in inputs:
+				f.write('The zenith angles of each source were chosen randomly between ' + str(inputs['zenith_min']) + ' and ' + str(inputs['zenith_max']) + ' degrees. ')
+			else:
+				raise RuntimeError("Must specify zenith angle(s) in input .yaml file.")
+			if 'azimuth' in inputs:
+				if type(inputs['azimuth']) == int:
+					f.write('All .source files have an azimuthal angle of ' + str(inputs['azimuth']) + ' degrees. ')
+				else:
+					f.write('The azimuthal angles of each source were chosen from the following list (in degrees): ' + str(inputs['azimuth']) + '. ')
+			elif 'azimuth_min' in inputs and 'azimuth_max' in inputs:
+				f.write('The azimuthal angles of each source were chosen randomly between ' + str(inputs['azimuth_min']) + ' and ' + str(inputs['azimuth_max']) + ' degrees. ')
+			else:
+				raise RuntimeError("Must specify azimuthal angle(s) in input .yaml file.")
+		
+		else:
+			raise RuntimeError("Only detector coordinates are supported for now. 'coordinate_system' in input .yaml file must be 'local'.")
+
+		if 'ph_flux' in inputs:
+			if type(inputs['ph_flux']) == int:
+				f.write('All .source files have a flux of ' + str(inputs['ph_flux']) + ' ph/cm<sup>2</sup>/s. ')
+			else:
+				f.write('The fluxes of each source were chosen from the following list (in ph/cm<sup>2</sup>/s): ' + str(inputs['ph_flux']) + '. ')
+		elif 'ph_flux_min' in inputs and 'ph_flux_max' in inputs:
+			f.write('The fluxes of each source were chosen randomly between ' + str(inputs['ph_flux_min']) + ' and ' + str(inputs['ph_flux_max']) + ' ph/cm<sup>2</sup>/s. ')
+		elif 'e_flux' in inputs:
+			if type(inputs['flux']) == int:
+				f.write('All .source files have a flux of ' + str(inputs['e_flux']) + ' erg/cm<sup>2</sup>/s. ')
+			else:
+				f.write('The fluxes of each source were chosen from the following list (in erg/cm<sup>2</sup>/s): ' + str(inputs['e_flux']) + '. ')
+		elif 'e_flux_min' in inputs and 'e_flux_max' in inputs:
+			f.write('The fluxes of each source were chosen randomly between ' + str(inputs['e_flux_min']) + ' and ' + str(inputs['e_flux_max']) + ' erg/cm<sup>2</sup>/s. ')
+		else:
+			raise RuntimeError("Must specify flux(es) in input .yaml file.")
 
 # Create list of event names and directories with spectrum and lightcurve file names
 def make_event_dict(input_path, mix_or_match, spectrum_type):
@@ -57,6 +100,7 @@ def make_event_dict(input_path, mix_or_match, spectrum_type):
 				spectrum_dict[item] = filename
 		else:
 			raise RuntimeError("'spectrum_type' must be either 'yaml' or 'dat'.")
+	
 	elif mix_or_match == 'mix':
 		spectrum_list = []
 		for file in os.listdir(input_path):
@@ -73,17 +117,119 @@ def make_event_dict(input_path, mix_or_match, spectrum_type):
 			index = np.random.randint(len(spectrum_list))
 			filename = spectrum_list[index]
 			spectrum_dict[item] = filename
+	
 	else:
 		raise RuntimeError("'mix_or_match' must be either 'mix' or 'match'.")
 
 	return key_list, lightcurve_dict, spectrum_dict
 
-# Defines zenith, azimuth, and flux of source
-def define_angles_flux(z_min, z_max, a_min, a_max, flux_min, flux_max):
+# Band function
+def band_function(e, alpha, beta, ebreak):
 
-	zenith = np.random.uniform(float(z_min), float(z_max))
-	azimuth = np.random.uniform(float(a_min), float(a_max))
-	flux = np.random.uniform(float(flux_min), float(flux_max))
+	if e <= (alpha - beta) * ebreak:
+		return (e / 100)**alpha * np.exp(-e / ebreak)
+	else:
+		return (e / 100)**beta * np.exp(beta - alpha) * ((alpha - beta) * ebreak / 100)**(alpha - beta)
+
+# Comptonized function
+def comp_function(e, index, epeak):
+
+	return e**index * np.exp(-(index + 2) * e / epeak)
+
+# Power law function
+def powerlaw_function(e, index):
+
+	return e**(-index)
+
+# Broken power law function
+def brokenpowerlaw_function(e, ebreak, index_lo, index_hi, e_max):
+
+	if e <= ebreak:
+		return e**(-index_lo)
+	else:
+		return e**(-index_hi) * emax**(index_hi - index_lo)
+
+# Calculate photon flux from energy flux
+def calc_flux(spectrum, e_flux):
+
+	if spectrum['type'] == 'File':
+		raise RuntimeError("If using spectrum .dat file, must define photon flux in input .yaml file.")
+	elif spectrum['type'] == 'Mono':
+		return e_flux * 6.24150647e8 / spectrum['energy']
+	elif spectrum['type'] == 'Band':
+		band_func = lambda e: band_function(e, spectrum['alpha'], spectrum['beta'], spectrum['ebreak'])
+		return e_flux * 6.24150647e8 / integrate.quad(band_func, spectrum['energy_min'], spectrum['energy_max'])[0]
+	elif spectrum['type'] == 'Comptonized':
+		comp_func = lambda e: comp_function(e, spectrum['index'], spectrum['epeak'])
+		return e_flux * 6.24150647e8 / integrate.quad(comp_func, spectrum['energy_min'], spectrum['energy_max'])[0]
+	elif spectrum['type'] == 'PowerLaw':
+		pl_func = lambda e: powerlaw_function(e, spectrum['index'])
+		return e_flux * 6.24150647e8 / integrate.quad(pl_func, spectrum['energy_min'], spectrum['energy_max'])[0]
+	elif spectrum['type'] == 'BrokenPowerLaw':
+		bpl_func = lambda e: brokenpowerlaw_function(e, spectrum['ebreak'], spectrum['index_lo'], spectrum['index_hi'], spectrum['energy_max'])
+		return e_flux * 6.24150647e8 / integrate.quad(bpl_func, spectrum['energy_min'], spectrum['energy_max'])[0]
+
+# Defines zenith, azimuth, and flux of source
+def define_angles_flux(inputs, spectrum):
+
+	if inputs['coordinate_system'] == 'local':
+		if 'zenith' in inputs:
+			if type(inputs['zenith']) == int or type(inputs['zenith']) == float:
+				zenith = float(inputs['zenith'])
+			elif type(inputs['zenith']) == list:
+				zenith = float(np.random.choice(inputs['zenith']))
+			else:
+				raise RuntimeError("'zenith' must be a number or list of numbers.")
+		elif 'zenith_min' in inputs and 'zenith_max' in inputs:
+			z_min = float(inputs['zenith_min'])
+			z_max = float(inputs['zenith_max'])
+			zenith = np.random.uniform(z_min, z_max)
+		else:
+			raise RuntimeError("Must specify zenith angle(s) in input .yaml file.")
+
+		if 'azimuth' in inputs:
+			if type(inputs['azimuth']) == int or type(inputs['azimuth']) == float:
+				azimuth = float(inputs['azimuth'])
+			elif type(inputs['azimuth']) == list:
+				azimuth = float(np.random.choice(inputs['azimuth']))
+			else:
+				raise RuntimeError("'azimuth' must be a number or list of numbers.")
+		elif 'azimuth_min' in inputs and 'azimuth_max' in inputs:
+			a_min = float(inputs['azimuth_min'])
+			a_max = float(inputs['azimuth_max'])
+			azimuth = np.random.uniform(a_min, a_max)
+		else:
+			raise RuntimeError("Must specify azimuthal angle(s) in input .yaml file.")
+
+	else:
+		raise RuntimeError("Only detector coordinates are supported for now. 'coordinate_system' in input .yaml file must be 'local'.")
+
+	if 'ph_flux' in inputs:
+		if type(inputs['ph_flux']) == int or type(inputs['ph_flux']) == float:
+			flux = float(inputs['ph_flux'])
+		elif type(inputs['ph_flux']) == list:
+			flux = float(np.random.choice(inputs['ph_flux']))
+		else:
+			raise RuntimeError("'ph_flux' must be a number or list of numbers.")
+	elif 'ph_flux_min' in inputs and 'ph_flux_max' in inputs:
+		flux_min = float(inputs['ph_flux_min'])
+		flux_max = float(inputs['ph_flux_max'])
+		flux = np.random.uniform(flux_min, flux_max)
+	elif 'e_flux' in inputs:
+		if type(inputs['e_flux']) == int or type(inputs['e_flux']) == float:
+			flux = float(calc_flux(spectrum, inputs['e_flux']))
+		elif type(inputs['e_flux']) == list:
+			e_flux = float(np.random.choice(inputs['e_flux']))
+			flux = float(calc_flux(spectrum, e_flux))
+		else:
+			raise RuntimeError("'e_flux' must be a number or list of numbers.")
+	elif 'e_flux_min' in inputs and 'e_flux_max' in inputs:
+		e_flux_min = float(inputs['e_flux_min'])
+		e_flux_max = float(inputs['e_flux_max'])
+		e_flux = np.random.uniform(e_flux_min, e_flux_max)
+		flux = float(calc_flux(spectrum, e_flux))
+	else:
+		raise RuntimeError("Must specify flux(es) in input .yaml file.")
 
 	return zenith, azimuth, flux
 
@@ -123,47 +269,51 @@ def lightcurve_spectrum_text(name, lightcurve, event_spectrum, path):
 
 	spectrum_text = define_spectrum(spectrum, name)
 
-	return spectrum_text, lightcurve_text
+	return spectrum_text, lightcurve_text, spectrum
 
 # Creates source file
-def make_source_file(event, filename, geometry, shield_counts, z, a, flux, spectrum_text, lightcurve_text):
+def make_source_file(event, filename, geometry, shield_counts, z, a, flux, spectrum_text, lightcurve_text, coordsys):
 
-	with open(filename, 'w') as f:
-		f.write('# Global Parameters\n')
-		f.write('Version                     1\n')
-		f.write('Geometry                    ' + geometry + '\n')
-		f.write('\n# Physics list\n')  
-		f.write('PhysicsListEM               LivermorePol\n')
-		f.write('\n# Output formats\n')
-		f.write('StoreSimulationInfo         init-only\n')
-		if shield_counts == 'y':
-			f.write('\n# Store shield counts\n')
-			f.write('PreTriggerMode              EveryEventWithHits\n')
-		f.write('\n# Run and source parameters\n')
-		f.write('Run                         GRBSim\n')
-		f.write('GRBSim.FileName             ' + event + '\n')
-		f.write('GRBSim.Time                 5000.0\n')
-		f.write('GRBSim.Source               ' + event + '\n')
-		f.write(event + '.ParticleType    1\n')
-		f.write(event + '.Beam            FarFieldPointSource ' + str(z) + ' ' + str(a) + '\n')
-		f.write('\n# Spectrum \n')
-		f.write(spectrum_text + '\n')
-		f.write('\n# Average photon flux in photon/cm2/s\n')
-		f.write(event + '.Flux            ' + str(flux) + '\n')
-		f.write('\n# Lightcurve\n')
-		f.write(lightcurve_text)
+	if coordsys == 'local':
+		with open(filename, 'w') as f:
+			f.write('# Global Parameters\n')
+			f.write('Version                     1\n')
+			f.write('Geometry                    ' + geometry + '\n')
+			f.write('\n# Physics list\n')  
+			f.write('PhysicsListEM               LivermorePol\n')
+			f.write('\n# Output formats\n')
+			f.write('StoreSimulationInfo         init-only\n')
+			if shield_counts == 'y':
+				f.write('\n# Store shield counts\n')
+				f.write('PreTriggerMode              EveryEventWithHits\n')
+			f.write('\n# Run and source parameters\n')
+			f.write('Run                         GRBSim\n')
+			f.write('GRBSim.FileName             ' + event + '\n')
+			f.write('GRBSim.Time                 5000.0\n')
+			f.write('GRBSim.Source               ' + event + '\n')
+			f.write(event + '.ParticleType    1\n')
+			f.write(event + '.Beam            FarFieldPointSource ' + str(z) + ' ' + str(a) + '\n')
+			f.write('\n# Spectrum \n')
+			f.write(spectrum_text + '\n')
+			f.write('\n# Average photon flux in photon/cm2/s\n')
+			f.write(event + '.Flux            ' + str(flux) + '\n')
+			f.write('\n# Lightcurve\n')
+			f.write(lightcurve_text)
+	else:
+		raise RuntimeError("Only detector coordinates are supported for now. 'coordinate_system' in input .yaml file must be 'local'.")
 
 
 def main():
 	input_file = parse_args().yaml
 	inputs = read_yaml(input_file)
 	input_path, output_path = define_paths(inputs)
+	write_readme(inputs, input_path, output_path)
 	events, lightcurves, spectra = make_event_dict(input_path, inputs['mix_or_match'], inputs['spectrum_type'])
 	for event in events:
 		source_filename = output_path + event + '.source'
-		zenith_angle, azimuthal_angle, flux = define_angles_flux(inputs['incidence_angle_min'], inputs['incidence_angle_max'], 0, 360, inputs['flux_min'], inputs['flux_max'])
-		spectrum_text, lightcurve_text = lightcurve_spectrum_text(event, lightcurves[event], spectra[event], input_path)
-		make_source_file(event, source_filename, inputs['geometry_path'], inputs['shield_counts'], zenith_angle, azimuthal_angle, flux, spectrum_text, lightcurve_text)
+		spectrum_text, lightcurve_text, spectrum = lightcurve_spectrum_text(event, lightcurves[event], spectra[event], input_path)
+		zenith_angle, azimuthal_angle, flux = define_angles_flux(inputs, spectrum)
+		make_source_file(event, source_filename, inputs['geometry_path'], inputs['shield_counts'], zenith_angle, azimuthal_angle, flux, spectrum_text, lightcurve_text, inputs['coordinate_system'])
 
 if __name__ == "__main__":
     main()
