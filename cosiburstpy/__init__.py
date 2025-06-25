32063af11ba98133db3e826d538937ed93696451
Eliza Neights <eneights@gmail.com>
2025-06-25 12:49:43 -0400

    Fix gbm import

--- cosiburstpy/__init__.py
+++ cosiburstpy/__init__.py
@@ -2,4 +2,10 @@ from .utility import write_readme, parse_args, read_yaml, write_yaml, read_csv, 
 from .event import Event, Lightcurve, SourceFile, Spectrum
 from .simulations import ACSData, EventTable, SpacecraftOrientation
 from .megalib import simulate, reconstruct, extract
-from .gbm import BayesianBlocks, DataDownload, DefineSpectrum
+
+try:
+	from .gbm import BayesianBlocks, DataDownload, DefineSpectrum
+except:
+	import logging
+	logger = logging.getLogger(__name__)
+	logger.warning('Unable to load gbm module.')
